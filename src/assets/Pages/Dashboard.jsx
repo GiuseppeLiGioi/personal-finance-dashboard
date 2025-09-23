fb26df634bb334da17f93ed7159216863cff55b7
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-09-23 15:01:38 +0200

    added. dashboard page structure

--- src/assets/Pages/Dashboard.jsx
+++ src/assets/Pages/Dashboard.jsx
@@ -1,5 +1,63 @@
-export default function Dashboard(){
-    return(
-        <h2>Sono Dashboard</h2>
+export default function Dashboard() {
+    return (
+        <div className="container-fluid px-4">
+
+            <div className="row row-cols-1 row-cols-md-4 justify-content-center g-3">
+                <div className="col">
+                    <div className="card">
+                        <div className="card-body">
+                            <h2 className="dashboard-card-title">Titolo</h2>
+                            <p className="dashboard-card-p">testo...</p>
+                        </div>
+                        <div className="container-graphic">
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col">
+                    <div className="card">
+                        <div className="card-body">
+                            <h2 className="dashboard-card-title">Titolo</h2>
+                            <p className="dashboard-card-p">testo...</p>
+                        </div>
+                        <div className="container-graphic">
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col">
+                    <div className="card">
+                        <div className="card-body">
+                            <h2 className="dashboard-card-title">Titolo</h2>
+                            <p className="dashboard-card-p">testo...</p>
+                        </div>
+                        <div className="container-graphic">
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="col">
+                    <div className="card">
+                        <div className="card-body">
+                            <h2 className="dashboard-card-title">Titolo</h2>
+                            <p className="dashboard-card-p">testo...</p>
+                        </div>
+                        <div className="container-graphic">
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
     )
 }
