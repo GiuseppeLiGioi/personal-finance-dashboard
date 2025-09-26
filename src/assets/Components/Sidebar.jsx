9f98b2808fe70180c46bb8cf31d81f704b6ded78
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-09-26 15:27:11 +0200

    added. responsive style at SideBar.jsx

--- src/assets/Components/Sidebar.jsx
+++ src/assets/Components/Sidebar.jsx
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom"
 
 export default function Sidebar(){
     return(
-        <div className="sidebar col-md-2 d-none d-md-block">
+        <div className="sidebar-desk-wrapper sidebar col-md-2 d-none d-md-block">
             <nav className="nav flex-column">
-                <NavLink className="nav-link" to="/">Dashboard</NavLink>
-                <NavLink className="nav-link" to="/transactions">Transazioni</NavLink>
-                <NavLink className="nav-link" to="/settings">Impostazioni</NavLink>
+                <NavLink className="link-sidebar nav-link" to="/">Dashboard</NavLink>
+                <NavLink className="link-sidebar nav-link" to="/transactions">Transazioni</NavLink>
+                <NavLink className="link-sidebar nav-link" to="/settings">Impostazioni</NavLink>
             </nav>
 
         </div>
